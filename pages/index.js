b2e641173cfd5eaf73f3a32712cfdf0e3366a59d
Sofia <sofibareta@gmail.com>
2023-06-27 17:14:23 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen
+
+// import React from 'react'
+// import Link from '../src/components/Link'
+// import Footer from '../src/components/patterns/Footer';
+
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>{children}</Tag>
+//             <style jsx> {`
+//                     ${Tag} {
+//                         color: red;
+//                     }
+//                 `}
+//             </style>
+//         </>
+//     )
+// }
+
+// export default function Homepage() {
+//     return (
+//         <div>
+//             <Title as="h1">Alura Cases - Homepage</Title>
+//             <Link href="/faq">FAQ</Link>
+//             <Footer />
+//         </div>
+//     )
+// }
